f69c9ee27f3dff4815d210953c5e160cafca153b
Wanlace Yates <wcy00001@mix.wvu.edu>
2022-09-09 19:46:00 -0400

    Lab 1: Add navbar header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './header/navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    FooterComponent
+],
   imports: [
     BrowserModule,
     AppRoutingModule
